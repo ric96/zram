2a8771dfe02aab79512be945ecbcaa99049b68a7
Sahaj Sarup <sahajsarup@gmail.com>
2017-09-13 20:28:44 +0530

    Update zram.sh

--- zram.sh
+++ zram.sh
@@ -2,7 +2,7 @@
 modprobe zram num_devices=4
 
 totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
-mem=$(( ($totalmem)* 1024))
+mem=$(( ($totalmem)* 1024 * 3))
 
 echo $mem > /sys/block/zram0/disksize
 
